d85c49e9860c2d0fe9a9560526d98ece2e81594d
sven0726 <sven0726@gmail.com>
2016-11-29 13:18:37 +0800

    Golang implementation

--- dashboard.v1/back/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Layout
-
-
-class LayoutAdmin(admin.ModelAdmin):
-    list_display = ('dataSet', 'layout', 'datetime',)
-
-
-admin.site.register(Layout, LayoutAdmin)
---
+++ dashboard.v1/build.sh
@@ -0,0 +1,3 @@
+docker build --rm --no-cache=true -t report:latest .
+#kirk images push report
+
