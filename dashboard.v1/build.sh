962e5e9b7a71ea229d7fcd779cf271860185fe4e
sven0726 <sven0726@gmail.com>
2016-11-29 13:52:35 +0800

    fix docker

--- dashboard.v1/build.sh
+++ dashboard.v1/build.sh
@@ -1,3 +1,8 @@
-docker build --rm --no-cache=true -t report:latest .
+path=`pwd`
+rm -rf dist && mkdir dist
+cp front/index.html dist/
+cp -r front/static dist/
+cd src/qiniu.com/report/; CGO_ENABLED=0 go build -a -o ../../../dist/reportd
+cd $path;docker build --rm --no-cache=true -t report:latest .
 #kirk images push report
 
