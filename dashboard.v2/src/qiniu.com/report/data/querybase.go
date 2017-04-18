f8bc4b89a2b7f7ef1c63c25c1987c29fa42b1907
sven0726 <sven0726@gmail.com>
2017-04-18 11:01:11 +0800

    统一数据源api

--- dashboard.v2/src/qiniu.com/report/data/querybase.go
+++ dashboard.v2/src/qiniu.com/report/data/querybase.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/qiniu/log.v1"
 
@@ -28,10 +29,85 @@ func NewExecutor(colls *common.Collections, restUrls []string) *Executor {
 	}
 }
 
+type RetTables struct {
+	Tables []map[string]string `json:"tables"`
+}
+
+func (e *Executor) ShowTables(ds common.DataSource) (ret RetTables, err error) {
+	// "SELECT TABLE_NAME, TABLE_TYPE FROM INFORMATION_SCHEMA.`TABLES` where TABLE_SCHEMA='192_168_0_102_wo.test';"
+	dbSpec := fmt.Sprintf("%s_%s.%s", ds.AppUri, ds.Name, ds.DbName)
+	sql := fmt.Sprintf("SELECT TABLE_NAME, TABLE_TYPE FROM INFORMATION_SCHEMA.`TABLES` where TABLE_SCHEMA='%s'", dbSpec)
+	log.Debugf("query SQL[%s]", sql)
+
+	ret = RetTables{}
+	res, err := e.client.Query(sql)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	for _, v := range res.Rows {
+		ret.Tables = append(ret.Tables,
+			map[string]string{
+				"name": v["TABLE_NAME"].(string),
+				"type": v["TABLE_TYPE"].(string)},
+		)
+	}
+	log.Infof("success to get %d tables, from schema %s", len(res.Rows), dbSpec)
+
+	return
+}
+
+func (m *Executor) getFieldTypeFromSourceType(src string) string {
+	numBerTypes := []string{"BIGINT", "DOUBLE", "INTEGER", "DECIMAL", "BOOLEAN"} //BOOLEAN暂时归Number
+	stringTypes := []string{"CHARACTER", "ANY"}                                  //ANY暂时归String
+	dateTypes := []string{"TIMESTAMP"}
+	tmp := strings.ToUpper(src)
+	for _, v := range numBerTypes {
+		if strings.HasPrefix(tmp, v) {
+			return FIELD_TYPE_NUMBER
+		}
+	}
+	for _, v := range stringTypes {
+		if strings.HasPrefix(tmp, v) {
+			return FIELD_TYPE_STRING
+		}
+	}
+	for _, v := range dateTypes {
+		if strings.HasPrefix(tmp, v) {
+			return FIELD_TYPE_TIMESTAMP
+		}
+	}
+	return tmp
+}
+
+func (e *Executor) GetTableShema(ds common.DataSource, tableName string) (ret []map[string]string, err error) {
+	// "SELECT COLUMN_NAME,DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS where TABLE_SCHEMA='192_168_0_102_wo.test' and TABLE_NAME='sales'"
+	dbSpec := fmt.Sprintf("%s_%s.%s", ds.AppUri, ds.Name, ds.DbName)
+	sql := fmt.Sprintf("SELECT COLUMN_NAME,DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='%s' AND TABLE_NAME='%s'", dbSpec, tableName)
+	log.Debugf("query SQL[%s]", sql)
+
+	ret = make([]map[string]string, 0)
+	res, err := e.client.Query(sql)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	for _, v := range res.Rows {
+		ret = append(ret,
+			map[string]string{
+				"field": v["COLUMN_NAME"].(string),
+				"type":  e.getFieldTypeFromSourceType(v["DATA_TYPE"].(string))},
+		)
+	}
+	log.Infof("success to get table info, from schema %s", dbSpec)
+	return
+}
+
 func (e *Executor) GetDataByDataSource(ds common.DataSource, tableName string, limit uint64) (ret rest.Results, err error) {
 	tableSpec := fmt.Sprintf("%s_%s.%s.%s", ds.AppUri, ds.Name, ds.DbName, tableName)
 	sql := fmt.Sprintf("SELECT * FROM %s LIMIT %d", tableSpec, limit)
 	log.Debugf("query SQL[%s]", sql)
+
 	ret, err = e.client.Query(sql)
 	if err != nil {
 		log.Error(err)
