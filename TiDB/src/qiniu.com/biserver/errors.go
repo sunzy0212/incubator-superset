cd1d8dec0606db6269072f5af3d51b5016a98347
mengjinglei <mengjinglei@qiniu.com>
2017-08-30 12:07:44 +0800

    update error code

--- TiDB/src/qiniu.com/biserver/errors.go
+++ TiDB/src/qiniu.com/biserver/errors.go
@@ -3,7 +3,7 @@ package biserver
 import "github.com/qiniu/http/httputil.v1"
 
 var (
-	ErrHeaderAppIdError      = httputil.NewError(400, "E7001: The Appid in request header is excepted to be a number!")
+	ErrHeaderAppIdError      = httputil.NewError(400, "E8001: The Appid in request header is excepted to be a number!")
 	ErrDBNameInvalidError    = httputil.NewError(400, "E8002: DB name is invalid")
 	ErrInvalidSqlError       = httputil.NewError(400, "E8003: invalid sql")
 	ErrInternalServerError   = httputil.NewError(599, "E9001: Server Internal Error")
