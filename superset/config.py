7a2e21f5f75dc78a939c581b3f6d17529b4b6574
mengjinglei <mengjinglei@qiniu.com>
2017-09-30 13:08:14 +0800

    fix secrete key

--- superset/config.py
+++ superset/config.py
@@ -49,8 +49,7 @@ CUSTOM_SECURITY_MANAGER = None
 # ---------------------------------------------------------
 
 # Your App secret key
-# SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'  # noqa
-SECRET_KEY = 'thisispandorasupersetsecretkey311'
+SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'  # noqa
 # The SQLAlchemy connection string.
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(DATA_DIR, 'superset.db')
 SQLALCHEMY_DATABASE_URI = 'mysql://root:@10.200.20.40:3306/super'
