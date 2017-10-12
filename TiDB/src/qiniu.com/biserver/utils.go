60d5f5334409629c36ad964ea9f2e3614d0804f6
mengjinglei <mengjinglei@qiniu.com>
2017-10-12 14:14:20 +0800

    修复了PDR-4635

--- TiDB/src/qiniu.com/biserver/utils.go
+++ TiDB/src/qiniu.com/biserver/utils.go
@@ -7,6 +7,7 @@ import (
 	"encoding/base64"
 	"fmt"
 	"strings"
+	"time"
 
 	"database/sql"
 
@@ -141,4 +142,138 @@ func getEncryptPasswordForDB(secretKey, p string) (password string, err error) {
 	ciphertext := make([]byte, len(pwd))
 	mode.CryptBlocks(ciphertext, pwd)
 	return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+const (
+	IS_DTTM = iota
+	IS_ACTIVE
+	GROUPBY
+	COUNT_DISTINCT
+	SUM
+	MIN
+	MAX
+	FILTERABLE
+	AVG
+	COLUMN_CONST_LENGTH
+)
+
+func generateSqlMetrics(columnName, columnType, appId, databaseName, tableName string) (metricValues string, err error) {
+
+	columnType = strings.Split(strings.ToUpper(columnType), "(")[0]
+	metrics := []string{"avg", "min", "max", "sum"}
+	template := `(
+"%s",
+"%s",
+"%s",
+"%s",
+"%s",
+(select id from tables where table_name='%s' and qiniu_uid=%s and database_id=(select id from dbs where qiniu_uid=%s and database_name='%s')),
+"%s",
+(select id from ab_user where username = '%s'),
+(select id from ab_user where username = '%s')
+)`
+
+	now := time.Now().Format("2006-01-02 15:04:05")
+
+	metricValues += fmt.Sprintf(template,
+		now,
+		now,
+		"count",
+		"COUNT(*)",
+		"count",
+		tableName, appId, appId, databaseName,
+		"COUNT(*)",
+		appId,
+		appId,
+	)
+	switch columnType {
+	case "INT", "BIGINT", "FLOAT", "DOUBLE":
+		for _, metric := range metrics {
+			metricValues += ","
+			metricValues += fmt.Sprintf(template,
+				now,
+				now,
+				fmt.Sprintf("%s__%s", metric, columnName),
+				fmt.Sprintf("%s__%s", metric, columnName),
+				metric,
+				tableName, appId, appId, databaseName,
+				fmt.Sprintf("%s(%s.%s)", strings.ToUpper(metric), tableName, columnName),
+				appId,
+				appId,
+			)
+
+		}
+	case "VARCHAR", "DATETIME":
+		//do nothing
+	default:
+		err = fmt.Errorf("unknown column type %s", columnType)
+		return
+	}
+	return
+}
+
+func generateTableColumns(columnName, columnType, appId, databaseName, tableName string) (columnValue string, err error) {
+	dict := make([]int, COLUMN_CONST_LENGTH)
+	columnType = strings.Split(strings.ToUpper(columnType), "(")[0]
+
+	switch columnType {
+	case "VARCHAR":
+		dict[GROUPBY] = 1
+		dict[FILTERABLE] = 1
+		dict[IS_ACTIVE] = 1
+	case "DATETIME":
+		dict[IS_ACTIVE] = 1
+		dict[IS_DTTM] = 1
+	case "INT", "DOUBLE", "BIGINT":
+		dict[IS_ACTIVE] = 1
+		dict[SUM] = 1
+		dict[MIN] = 1
+		dict[MAX] = 1
+		dict[AVG] = 1
+	default:
+		err = fmt.Errorf("unknown column type %s", columnType)
+		return
+	}
+
+	now := time.Now().Format("2006-01-02 15:04:05")
+
+	template := `(
+"%s",
+"%s",
+(select id from tables where table_name='%s' and qiniu_uid=%s and database_id=(select id from dbs where qiniu_uid=%s and database_name='%s')),
+"%s",
+%d,
+%d,
+"%s",
+%d,
+%d,
+%d,
+%d,
+%d,
+%d,
+(select id from ab_user where username = '%s'),
+(select id from ab_user where username = '%s'),
+%d
+)`
+	return fmt.Sprintf(template,
+		now,
+		now,
+		tableName,
+		appId,
+		appId,
+		databaseName,
+		columnName,
+		dict[IS_DTTM],
+		dict[IS_ACTIVE],
+		columnType,
+		dict[GROUPBY],
+		dict[COUNT_DISTINCT],
+		dict[SUM],
+		dict[MAX],
+		dict[MIN],
+		dict[FILTERABLE],
+		appId,
+		appId,
+		dict[AVG],
+	), nil
 }
