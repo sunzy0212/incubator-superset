dfe6c13d612898d7f4b3ce99eba50c1337814b57
sven0726 <sven0726@gmail.com>
2017-10-23 15:43:34 +0800

    汉化

--- superset/assets/javascripts/SqlLab/components/QueryHistory.jsx
+++ superset/assets/javascripts/SqlLab/components/QueryHistory.jsx
@@ -15,9 +15,10 @@ const QueryHistory = (props) => {
     return (
       <QueryTable
         columns={[
-          'state', 'started', 'duration', 'progress',
-          'rows', 'sql', 'output', 'actions',
-        ]}
+        {key:'state', label:"状态"}, {key:'started', label:"开始时间"}, {key:'duration', label:"执行时长"},
+        {key:'progress', label:"进度"}, {key:'rows', label:"返回结果条数"}, {key:'sql', label:"查询语句"},
+        {key:'output', label:"输出"}, {key:'actions', label:"操作"}
+       ]}
         queries={props.queries}
         actions={props.actions}
       />
